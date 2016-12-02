fce6833a265f5b0d698a6e511a6114e674ef88f6
Duan <JDuan@mi.corp.rockfin.com>
2016-12-02 18:33:57 -0500

    Debug

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'version': '0.0.1',
+    'version': 'v1',
     'secret' : 'Password123!',
     'expiresIn' : '7d',
     'database' : 'mongodb://nodeuser:Password123!@ds044989.mlab.com:44989/mongo-dev'
